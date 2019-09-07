5ff35c37b11f27ef1e05f98029a11fcd01500293
Daniel Ferguson <dan.ferguson@outlook.com>
2019-09-07 11:22:02 -0500

    Finish about page

--- src/app/home/about/about.vm.ts
+++ src/app/home/about/about.vm.ts
@@ -1,13 +1,19 @@
 import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faMailBulk } from '@fortawesome/free-solid-svg-icons';
 
 export class AboutViewModel {
     currentEmployer = 'Sync1 Systems';
-    currentEmployerWUrl = '';
-    teamPath = '/blog/sync1-systems';
+    currentEmployerUrl = 'https://www.sync1systems.com/';
+
     githubIcon = faGithub;
     githubUrl = 'https://github.com/daniefer'
+
     linkedInIcon = faLinkedin;
     linkedInUrl = 'https://www.linkedin.com/in/ferguson-dan/'
+
     twitterIcon = faTwitter;
     twitterUrl = 'https://twitter.com/devdanaf'
+
+    emailIcon = faMailBulk;
+    email = 'dan.ferguson@outlook.com'
 }
